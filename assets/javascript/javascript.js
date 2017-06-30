ac47a1811c3f600e29a3db0b6ce8d0011e541adc
trcka <michael.trcka@ttu.edu>
2017-06-30 13:46:53 -0500

    logic almost done

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,30 +1,47 @@
-var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
 	//this is for user input
+	
+
+  
+$("button").on("click", function(){
+
+
 	var searchTerm=$("#searchInput").val();
 	var yearBegin=$("#startYear").val();
 	var yearEnd=$("#endYear").val();
-	var yearBeginCode=yearBegin+=0101
-	var yearEndCode=yearEnd+=0101
-	url += '?' + $.param({
-  	'api-key': "e85e5e2d0c1743efa02fc2149358728e",
-  	'q': searchTerm,
-  	'begin_date': yearBeginCode,
-  	'end_date': yearEndCode
-});
+	// var yearBeginCode=yearBegin+=0101
+	// var yearEndCode=yearEnd+=1230
 
-	console.log(url)
+
+
+
+
+
+
+
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+		url += '?' + $.param({
+  		'api-key': "e85e5e2d0c1743efa02fc2149358728e",
+  		'q': searchTerm,
+  		'begin_date': yearBegin,
+  		'end_date': yearEnd
+		});
+
+
 $.ajax({
   url: url,
   method: 'GET',
 }).done(function(responce){
-  
-$("button").on("click" function(){
-	console.log(responce);
-
-})
-  console.log(responce);
 
 
+	
+console.log(responce)
 
 
-})
+});
+
+
+
+
+
+});
